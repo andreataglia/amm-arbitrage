942dd7d001c8e0e1171541653d0e6e66a5eda31d
Andrea Taglia <andrea.taglia@pwc.com>
2021-04-30 11:56:48 +0200

    feat(1a - andrea): simple brute force algorithm with fixed breadth and depth with sample values

--- contracts/arbitrage-saga/arbitragePathAlgorithm.js
+++ contracts/arbitrage-saga/arbitragePathAlgorithm.js
@@ -10,24 +10,65 @@ const ammAggregatorAddress = "0x81391d117a03A6368005e447197739D06550D4CD";
 
 // abi of AMM Aggregator
 const AMMAggregator = abis.AMMAggregatorABI;
+let ammAggregator;
+
 // TODO: start by the token list, and retrieve the pools using byTokens (not for balancer where we need hard coded pools)
 //Pool list example. Liquidity Pool addresses on uniswap and mooniswap. [0][0] = uni[USDC-ETH]. [0][1] = mooni[ETH-USDC]
 // const poolPair = [["0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc", "0x61bb2fda13600c497272a8dd029313afdb125fd3"], ["0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852", "0xbeabef3fc02667d8bd3f702ae0bb2c4edb3640cc"], ["0xd3d2e2692501a5c9ca623199d38826e513033a17","0x798934cdcfae18764ef4819274687df3fb24b99b"], ["0xa478c2975ab1ea89e8196811f51a7b7ade33eb11", "0x75116bd1ab4b0065b44e1a4ea9b4180a171406ed"],["0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974","0x377d0c7ecf3f94968bcbf85de863282cae997b45"], ["0xc2adda861f89bbb333c90c492cb837741916a225", "0x3a5e247d1b931347ef6e75a11b28bc5bfb4f608e"],["0x31631b3dd6c697e574d6b886708cd44f5ccf258f", "0xae461ca67b15dc8dc81ce7615e0320da1a9ab8d5"]];
 //uni[USDC-ETH], uni[eth-usdt], uni[uni-eth], uni[dai-eth], uni[link-eth], uni[mkr-eth], uni[aave-eth, mooni[dai-usdc], uni[dai-usdc], mooni[usdc-eth], mooni[eth-usdt], mooni[uni-eth], mooni[dai-eth], mooni[link-eth], mooni[eth-maker]
 
-const tokensList = [context.usdtTokenAddress, context.daiTokenAddress];
 
-let ammAggregator;
+const BREADTH = 3;
+// algorithm DEPTH
+// const tokensList = [context.usdtTokenAddress, context.daiTokenAddress];
+const tokensList = ['a', 'b', 'c'];
 
-async function findBestArbitragePathForInputToken(inputTokenAddress, inputTokenAmount) {
+let initialTokenAddress;
+let initialTokenAmount;
+
+
+async function init(inputTokenAddress, inputTokenAmount){
     ammAggregator = new web3.eth.Contract(AMMAggregator, ammAggregatorAddress);
-
-    let prova = await calculateSingleSwapOutput(context.usdtTokenAddress, inputTokenAmount, context.daiTokenAddress);
-
-    const maxBreadthSearch = 2;
-    // try out all possible arbitrage paths with with depth limited by the token list, and breadth limited by a fixed constant
-
+    initialTokenAddress = inputTokenAddress;
+    initialTokenAmount = inputTokenAmount;
+    const result = await findBestArbitragePathForInputToken(initialTokenAddress, initialTokenAmount, []);
+    console.log(result);
 }
+
+// try out all possible arbitrage paths with with depth limited by the token list, and breadth limited by a fixed constant
+// TODO: so far it only returns the amount of the arbitrage, but it should return the whole arbitrage swap data path.
+async function findBestArbitragePathForInputToken(inputToken, inputTokenAmount, currentPath) {
+    // algorithm reached an end. swap back to initial token
+    if(currentPath.length === BREADTH - 1){
+        const newPath = currentPath.concat(initialTokenAddress);
+        return {
+            outputAmount: randomNumber(),
+            path: newPath
+        }
+    }
+
+    let results = [];
+    
+    for (let i = 0; i < tokensList.length; i++) {
+        const nextTokenToSwap = tokensList[i];   
+        if(nextTokenToSwap !== inputToken) {
+            const newPath = currentPath.concat(inputToken);
+            results.push(await findBestArbitragePathForInputToken(nextTokenToSwap, inputTokenAmount+randomNumber(), newPath));
+        }
+    }
+    
+    return results.reduce((a,b) => {
+        return a.outputAmount > b.outputAmount ? a : b;
+    })
+}
+
+
+// let prova = await calculateSingleSwapOutput(inputToken, inputTokenAmount, context.daiTokenAddress);
+function randomNumber(){
+    return Math.random();
+}
+
+
 // returns the token output for that pool, for that AMM
 // only single pairs calculation.
 async function calculateSingleSwapOutput(inputTokenAddress, inputTokenAmount, outputTokenAddress){
@@ -46,11 +87,8 @@ async function calculateSingleSwapOutput(inputTokenAddress, inputTokenAmount, ou
     return -1;
 }
 
-// // find the LP address from the locally stored ones
-// function getLocalLPAddress(tokenAddress0, tokenAddress1){
 
-// }
-
+// retrievs pools and tokens path to be used on a swap, given the tokens and the ammPLugin interested for swapping 
 async function retrieveSwapData(tokens, amm) {
     var swapPools = [];
     var swapTokens = [];
@@ -98,4 +136,4 @@ async function findAMMPluginByName(ammName){
 }
 
 // TODO: use utilities.toDecimals('10', '18'). need web3 injected first..
-findBestArbitragePathForInputToken('0x6b175474e89094c44da98b954eedeac495271d0f', '10000000000000000000');
+init('a', 1);
